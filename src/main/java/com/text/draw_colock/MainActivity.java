0113c07624774d5030ca324afd4b2f02d4afbc84
y-kaixin <1448488568@qq.com>
2019-09-23 22:18:39 +0800

    111

--- src/main/java/com/text/draw_colock/MainActivity.java
+++ src/main/java/com/text/draw_colock/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // 测试
     }
 
     private void initView() {
